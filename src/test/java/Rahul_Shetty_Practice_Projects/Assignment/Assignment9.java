551c0c0f2e3ff2f5bb2acd79619db013c054747a
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-12 09:30:19 +0530

    practice of assignments

---
+++ src/test/java/Rahul_Shetty_Practice_Projects/Assignment/Assignment9.java
@@ -0,0 +1,18 @@
+package Rahul_Shetty_Practice_Projects.Assignment;
+
+public class Assignment9 {
+	public static void main(String[] args) 
+	{
+		int k=3;
+		for (int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(k);
+				System.out.print("\t");
+				k=k+3;
+			}
+			System.out.println("");
+		}
+	}
+}
---
+++ src/test/java/Rahul_Shetty_Practice_Projects/Assignment/demo_interface.java
@@ -0,0 +1,9 @@
+package Rahul_Shetty_Practice_Projects.Assignment;
+
+public interface demo_interface {
+	
+	public void getdemo();
+	
+	public void setdemo(); 
+
+}
