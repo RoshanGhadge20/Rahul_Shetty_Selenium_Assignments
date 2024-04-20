a19c7267f11aa9efc338a12407fa35c50d8256ff
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-20 12:04:51 +0530

    practice of assignment 12

--- src/test/java/Rahul_Shetty_Practice_Projects/Assignment/Assignment12.java
+++ src/test/java/Rahul_Shetty_Practice_Projects/Assignment/Assignment12.java
@@ -3,22 +3,23 @@ package Rahul_Shetty_Practice_Projects.Assignment;
 import java.util.ArrayList;
 
 public class Assignment12 {
-    public static void main(String[] args) {
-        int a[] = {4, 5, 5, 5, 4, 6, 9, 4};
-        
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        int count=0;
-        for (int i = 0; i < a.length; i++) {
-            if (!al.contains(a[i])) {
-                al.add(a[i]);
-               
-				count++;
-            }
-        }
-        
-        for (Integer num : al) {
-        	/*if(count==1) {*/
-            System.out.println(num + " is a unique number");
-        }
-    }
+	public static void main(String[] args) {
+		int a[] = { 4, 5, 5, 5, 4, 6, 9, 4 };
+
+		ArrayList<Integer> uniqueNumbers = new ArrayList<>();
+
+		for (int i = 0; i < a.length; i++) {
+			if (uniqueNumbers.contains(a[i])) {
+				uniqueNumbers.remove((Integer) a[i]); // Remove from unique list if it repeats
+			} else {
+				uniqueNumbers.add(a[i]);
+			}
+		}
+		System.out.println(uniqueNumbers);
+		if (uniqueNumbers.size() == 1) {
+			System.out.println("Unique number is: " + uniqueNumbers.get(0));
+		} else {
+			System.out.println("No unique number found.");
+		}
+	}
 }
